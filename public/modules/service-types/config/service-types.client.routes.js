ff8ca5ae36a5ccd30c92e64ebf1b410a9a13c50a
FranTorletti <frantorletti@gmail.com>
2015-10-17 18:43:10 -0300

    advances

--- public/modules/service-types/config/service-types.client.routes.js
+++ public/modules/service-types/config/service-types.client.routes.js
@@ -5,19 +5,15 @@ angular.module('service-types').config(['$stateProvider',
 	function($stateProvider) {
 		// Service types state routing
 		$stateProvider.
-		state('listServiceTypes', {
+		state('list-service-ypes', {
 			url: '/service-types',
 			templateUrl: 'modules/service-types/views/list-service-types.client.view.html'
 		}).
-		state('createServiceType', {
+		state('create-service-type', {
 			url: '/service-types/create',
 			templateUrl: 'modules/service-types/views/create-service-type.client.view.html'
 		}).
-		state('viewServiceType', {
-			url: '/service-types/:serviceTypeId',
-			templateUrl: 'modules/service-types/views/view-service-type.client.view.html'
-		}).
-		state('editServiceType', {
+		state('edit-service-type', {
 			url: '/service-types/:serviceTypeId/edit',
 			templateUrl: 'modules/service-types/views/edit-service-type.client.view.html'
 		});
